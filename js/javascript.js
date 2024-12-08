bbf03c82d484b84bbf19510a0b41ad3c5cb46f7c
Oleksandr Neysalo <sayaee@gmail.com>
2024-12-08 20:23:03 +0200

    зміни у джс

--- js/javascript.js
+++ js/javascript.js
@@ -2,25 +2,36 @@
 
 alert('Hello my friend!');
 
-let userName = 'Alexander';
-console.log(userName);
-console.log(typeof userName);
+// Отримуємо три рядки від користувача
+let firstQuestion = prompt("Введіть перший рядок:");
+let twoQuestion = prompt("Введіть другий рядок:");
+let threeQuestion = prompt("Введіть третій рядок:");
 
-let userAge = 35;
-console.log(userAge);
-console.log(typeof userAge);
+// Зберігаємо рядки у масиві
+let questions = [firstQuestion, twoQuestion, threeQuestion];
 
-let isStudent = true;
-console.log(isStudent);
-console.log(typeof isStudent);
+// Виводимо їх у довільному порядку однією командою
+console.log(`${questions[2]}, ${questions[0]}, ${questions[1]}`);
 
-let userAddress = null;
-console.log(userAddress);
-console.log(typeof userAddress);
+// let userName = 'Alexander';
+// console.log(userName);
+// console.log(typeof userName);
 
-let userHobby;
-console.log(userHobby);
-console.log(typeof userHobby);
+// let userAge = 35;
+// console.log(userAge);
+// console.log(typeof userAge);
+
+// let isStudent = true;
+// console.log(isStudent);
+// console.log(typeof isStudent);
+
+// let userAddress = null;
+// console.log(userAddress);
+// console.log(typeof userAddress);
+
+// let userHobby;
+// console.log(userHobby);
+// console.log(typeof userHobby);
 
 // let result = prompt('Enter your name');
 // console.log(result);
