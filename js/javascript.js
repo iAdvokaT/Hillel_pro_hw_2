7b620bf17c3a5b6a6b8cf68d851a1d94e28f4dd6
Oleksandr Neysalo <sayaee@gmail.com>
2024-12-08 20:11:02 +0200

    add file

---
+++ js/javascript.js
@@ -0,0 +1,32 @@
+'use strict';
+
+alert('Hello my friend!');
+
+let userName = 'Alexander';
+console.log(userName);
+console.log(typeof userName);
+
+let userAge = 35;
+console.log(userAge);
+console.log(typeof userAge);
+
+let isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+let userAddress = null;
+console.log(userAddress);
+console.log(typeof userAddress);
+
+let userHobby;
+console.log(userHobby);
+console.log(typeof userHobby);
+
+// let result = prompt('Enter your name');
+// console.log(result);
+
+// result = confirm('Are you sure you want to be a happy?');
+// console.log(result);
+
+// console.log(typeof 42);
+// console.log(typeof 'Hello world!');
